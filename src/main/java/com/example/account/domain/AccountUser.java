f80b07926008b7e7678d44f18a17ff8249158bfc
jeralzzang <jeralzzang@naver.com>
2022-09-24 17:28:41 +0900

    commit(mac)

--- src/main/java/com/example/account/domain/AccountUser.java
+++ src/main/java/com/example/account/domain/AccountUser.java
@@ -1,6 +1,5 @@
 package com.example.account.domain;
 
-import com.example.account.type.AccountStatus;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
@@ -11,30 +10,20 @@ import java.time.LocalDateTime;
 
 @Getter
 @Setter
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 @Entity
 @EntityListeners(AuditingEntityListener.class)
-public class Account {
+public class AccountUser {
+
     @Id
     @GeneratedValue
     private Long id;
-
-    @ManyToOne
-    private AccountUser accountUser;
-    private String accountNUmber;
-
-    @Enumerated(EnumType.STRING)
-    private AccountStatus accountStatus;
-    private Long balance;
-
-    private LocalDateTime registeredAt;
-    private LocalDateTime unRegisteredAt;
+    private String name;
 
     @CreatedDate
     private LocalDateTime createdAt;
     @LastModifiedDate
     private LocalDateTime updatedAt;
-
 }
